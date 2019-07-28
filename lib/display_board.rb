471ef76674eafb4accfdd73f6a61b34b08d07605
energetic-port-0799 <m.tarrats@yahoo.com>
2019-07-28 17:54:20 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-  display_board
-    puts "Tic Tac Toe board"
+ def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
